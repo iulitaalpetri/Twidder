4cca8d5a154729b5086c723de3b8182f96b0901d
iulitaalpetri <92823812+iulitaalpetri@users.noreply.github.com>
2022-12-11 04:49:37 +0300

    push 6

--- Twidder/Controllers/CommentsController.cs
+++ Twidder/Controllers/CommentsController.cs
@@ -1,13 +1,63 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Twidder.Data;
+using Twidder.Models;
 
 namespace Twidder.Controllers
 {
     public class CommentsController : Controller
     {
-        public IActionResult Index()
+
+
+        private readonly ApplicationDbContext db;
+
+        public CommentsController(ApplicationDbContext context)
         {
-            return View();
-            
+            db = context;
         }
+
+        // Stergerea unui comentariu asociat unui articol din baza de date
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Comment comm = db.Comments.Find(id);
+            db.Comments.Remove(comm);
+            db.SaveChanges();
+            return Redirect("/Posts/Show/" + comm.PostId);
+        }
+
+        // In acest moment vom implementa editarea intr-o pagina View separata
+        // Se editeaza un comentariu existent
+
+        public IActionResult Edit(int id)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            return View(comm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Comment requestComment)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            if (ModelState.IsValid)
+            {
+
+                comm.Content = requestComment.Content;
+
+                db.SaveChanges();
+
+                return Redirect("/Posts/Show/" + comm.PostId);
+            }
+            else
+            {
+                return View(requestComment);
+            }
+
+        }
+
+
+
+       
     }
 }
--- Twidder/Controllers/PostsController.cs
+++ Twidder/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Twidder.Data;
 using Twidder.Models;
@@ -103,20 +104,99 @@ namespace Twidder.Controllers
                 return View(post);
             }
 
-            /*
-             try
-             {
-                 db.Articles.Add(article);
-                 db.SaveChanges();
-                 TempData["message"] = "Articolul a fost adaugat";
-                 return RedirectToAction("Index");
-             }
+            
+        }
 
-             catch (Exception)
-             {
-                 return View(article);
-             }
-             */
+
+        public IActionResult Edit(int id)
+        {
+
+            Post post = db.Posts.Include("User")
+                                        .Where(pst => pst.Id == id)
+                                        .First();
+
+            //article.Categ = GetAllCategories();
+
+            return View();
+
+        }
+
+        // Se adauga articolul modificat in baza de date
+        [HttpPost]
+        public IActionResult Edit(int id, Post requestPost)
+        {
+            Post post = db.Posts.Find(id);
+            //requestPost.Categ = GetAllCategories();
+
+            if (ModelState.IsValid)
+            {
+                
+                post.Content = requestPost.Content;
+                post.User = requestPost.User;
+                TempData["message"] = "Postarea a fost modificata";
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(requestPost);
+            }
+        }
+
+
+        // Se sterge un articol din baza de date 
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+
+            Post post = db.Posts.Find(id);
+            
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            TempData["message"] = "Postarea a fost stearsa";
+            return RedirectToAction("Index");
+        }
+
+        [NonAction]
+       // public IEnumerable<SelectListItem> GetAllCategories()
+        //{
+            // generam o lista de tipul SelectListItem fara elemente
+            //var selectList = new List<SelectListItem>();
+
+            // extragem toate categoriile din baza de date
+           // var users = from u in db.
+                            // select cat;
+
+            // iteram prin categorii
+           // foreach (var category in categories)
+            //{
+                // adaugam in lista elementele necesare pentru dropdown
+                // id-ul categoriei si denumirea acesteia
+                //selectList.Add(new SelectListItem
+                //{
+                    //Value = category.Id.ToString(),
+                  //  Text = category.CategoryName.ToString()
+                //});
+            //}
+            /* Sau se poate implementa astfel: 
+             * 
+            foreach (var category in categories)
+            {
+                var listItem = new SelectListItem();
+                listItem.Value = category.Id.ToString();
+                listItem.Text = category.CategoryName.ToString();
+
+                selectList.Add(listItem);
+             }*/
+
+
+            // returnam lista de categorii
+            //return selectList;
+        //}
+
+        public IActionResult IndexNou()
+        {
+            return View();
         }
 
     }
