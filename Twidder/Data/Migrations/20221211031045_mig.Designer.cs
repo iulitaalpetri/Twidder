a7db110e792214b7e67e41b3266bde7d23603e62
Annemarie Beatrix Messner <annemarie-beatrix.messner@s.unibuc.ro>
2022-12-11 05:44:07 +0200

    Views 2

--- Twidder/Data/Migrations/20221211031045_mig.Designer.cs
+++ Twidder/Data/Migrations/20221211031045_mig.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Twidder.Data;
 
@@ -11,9 +12,10 @@ using Twidder.Data;
 namespace Twidder.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221211031045_mig")]
+    partial class mig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
