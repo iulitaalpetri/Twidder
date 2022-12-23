d836e5993c3eb97c0ada266ff46daa27ab302ec3
iulitaalpetri <92823812+iulitaalpetri@users.noreply.github.com>
2022-12-23 07:15:08 +0300

    models_friend_req

---
+++ Twidder/Controllers/FriendsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Twidder.Controllers
+{
+    public class FriendsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Twidder/Models/Friend.cs
@@ -0,0 +1,20 @@
+﻿using NuGet.Packaging.Signing;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Twidder.Models
+{
+    public class Friend
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        public virtual ApplicationUser? AccountId { get; set; }
+
+        public virtual ApplicationUser? MyFriendAccountId { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public Timestamp Timestamp { get; set; }
+    }
+}
